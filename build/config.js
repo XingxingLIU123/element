d1297275c2cba6b97e6539c749a9442ec61b3a56
liuxing <100608706@qq.com>
2018-06-11 10:21:13 +0800

    fix

--- build/config.js
+++ build/config.js
@@ -10,21 +10,21 @@ var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'
 var externals = {};
 
 Object.keys(Components).forEach(function(key) {
-  externals[`element-ui/packages/${key}`] = `element-ui/lib/${key}`;
+  externals[`tb-element/packages/${key}`] = `tb-element/lib/${key}`;
 });
 
-externals['element-ui/src/locale'] = 'element-ui/lib/locale';
+externals['tb-element/src/locale'] = 'tb-element/lib/locale';
 utilsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/utils/${file}`] = `element-ui/lib/utils/${file}`;
+  externals[`tb-element/src/utils/${file}`] = `tb-element/lib/utils/${file}`;
 });
 mixinsList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/mixins/${file}`] = `element-ui/lib/mixins/${file}`;
+  externals[`tb-element/src/mixins/${file}`] = `tb-element/lib/mixins/${file}`;
 });
 transitionList.forEach(function(file) {
   file = path.basename(file, '.js');
-  externals[`element-ui/src/transitions/${file}`] = `element-ui/lib/transitions/${file}`;
+  externals[`tb-element/src/transitions/${file}`] = `tb-element/lib/transitions/${file}`;
 });
 
 externals = [Object.assign({
